d6b23825a6f93951476fa09d82eb6ebcacb5db53
WasTheStig <WasTheStig@gmail.com>
2025-04-15 14:40:43 -0700

    Update File1.py

--- File1.py
+++ File1.py
@@ -18,7 +18,7 @@ def generate_gcode(
     cmd = "G2" if weld_direction.upper() == "CW" else "G3"
 
     gcode.append("OPEN PROG 1 CLEAR")
-    gcode.append(f";Power Mode {laser_mode} {power_mode}w")
+    
     gcode.append(f";Power Mode {laser_mode} {power_mode}w")
     if pp_1_on:
         gcode.append(f";Point 1, Time {pp_1_t}s, Power {pp_1_p}%")
