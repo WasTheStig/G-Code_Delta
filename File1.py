eea0105720761ef63c502e8a12feec5c93a8ad61
WasTheStig <WasTheStig@gmail.com>
2025-04-15 15:36:31 -0700

    Update File1.py

--- File1.py
+++ File1.py
@@ -52,6 +52,11 @@ def generate_gcode(
     else:
         st.warning("Enter valid Feed Rate and Radius to calculate weld time.")
 
+     if overlap_pass == "Y":
+     weld_time *= 2
+     st.info(f"With overlap pass: {weld_time:.2f} seconds")
+
+
 
     
     if program_type == "TACK":
