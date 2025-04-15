a7f3c6fff76310623fa2bfaa92623a1913cd8477
WasTheStig <WasTheStig@gmail.com>
2025-04-15 15:28:58 -0700

    Update File1.py

--- File1.py
+++ File1.py
@@ -83,7 +83,7 @@ def generate_gcode(
         ]
         if overlap_pass.upper() == "Y":
             gcode.append(f"{cmd} X0 Y0 I0 J-{circle_radius} ;Overlap")
-        gcode.append("M55 ;Laser OFF","G90","x0y0")
+        gcode.append("M55 ;Laser OFF")
 
     elif program_type == "ROTARY":
         gcode += [
