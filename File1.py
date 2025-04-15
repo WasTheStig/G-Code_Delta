1e00b4fc7379be50e693f56e07dad3fa02cfb261
WasTheStig <WasTheStig@gmail.com>
2025-04-15 12:36:31 -0700

    Update File1.py

--- File1.py
+++ File1.py
@@ -18,11 +18,11 @@ def generate_gcode(
     cmd = "G2" if weld_direction.upper() == "CW" else "G3"
 
     gcode.append("OPEN PROG 1 CLEAR")
-    gcode.append(f" ;Power Mode CW {power_mode}%")
-    gcode.append(f" ;Point 1, Time {pp_1_t}s, Power {pp_1_p}%"
-    gcode.append(f" ;Point 2, Time {pp_2_t}s, Power {pp_2_p}%"
-    gcode.append(f" ;Point 3, Time {pp_3_t}s, Power {pp_3_p}%"
-    gcode.append(f" ;Point 1, Time {pp_4_t}s, Power {pp_4_p}%"    
+    gcode.append(f" Power Mode CW {power_mode}%")
+    gcode.append(f" Point 1, Time {pp_1_t}s, Power {pp_1_p}%"
+    gcode.append(f" Point 2, Time {pp_2_t}s, Power {pp_2_p}%"
+    gcode.append(f" Point 3, Time {pp_3_t}s, Power {pp_3_p}%"
+    gcode.append(f" Point 1, Time {pp_4_t}s, Power {pp_4_p}%"    
     gcode.append("p480=0")
     gcode.append("M36 ;Door Close")
     gcode.append(f"DWELL {initial_gas_delay} ;Cover Gas Delay")
